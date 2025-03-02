bf6f98030cda48586db4d17b38c2b3b324b3660d
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-03-02 04:48:58 -0800

    Inserting and Deleting Data

--- Backend/products.py
+++ Backend/products.py
@@ -23,7 +23,23 @@ def get_all_products(connection):
 
     return response
 
+def insert_new_product(connection,product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO product "
+             "(name,uom_id,price_per_unit)"
+             " values (%s, %s,%s)")
+    data = (product['product_name'], product['uom_id'], product['price_per_unit'])
+    cursor.execute(query,data)
+    connection.commit()
+    return cursor.lastrowid
+
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = "DELETE FROM product WHERE product_id = %s"
+    cursor.execute(query,(product_id,))
+    connection.commit()
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(get_all_products(connection))
+    print(delete_product(connection,3))
